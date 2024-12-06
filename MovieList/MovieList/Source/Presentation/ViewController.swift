e72ec40f9b00bda1720119b17273ea3c8ccd8666
JinUng41 <ingjwjw@naver.com>
2024-12-06 15:24:59 +0900

    add - 프로젝트 파일 추가

---
+++ MovieList/MovieList/Source/Presentation/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by 김진웅 on 12/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
